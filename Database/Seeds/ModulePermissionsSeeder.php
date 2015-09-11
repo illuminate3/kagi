f7358b14717c487c8d78c9069f812857b3b9443e
illuminate3 <carsten.sapporo@gmail.com>
2015-09-11 11:55:40 -0500

    dev 17.3.1

---
+++ Database/Seeds/ModulePermissionsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Modules\Kagi\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+Use DB;
+use Schema;
+
+
+class ModuleSeeder extends Seeder {
+
+	public function run()
+	{
+
+// Permissions -------------------------------------------------------------
+		$permissions = array(
+			[
+				'name'				=> 'Manage Users',
+				'slug'				=> 'manage_kagi',
+				'description'		=> 'Give permission to user to Manage Users.'
+			],
+		 );
+
+		if (Schema::hasTable('permissions'))
+		{
+			DB::table('permissions')->insert( $permissions );
+		}
+
+	} // run
+
+}
