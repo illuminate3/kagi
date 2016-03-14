177076f8d3df6b35283b2efb6a36f55ae74fc3e5
illuminate3 <carsten.sapporo@gmail.com>
2016-03-13 23:01:34 -0500

    dev 17.9.2

--- Resources/Views/social/login.blade.php
+++ Resources/Views/social/login.blade.php
@@ -8,7 +8,7 @@
 <div id="flex-item">
 
 
-	<img src="{{ asset('themes/district/assets/not_used/DistrictSeal.png') }}">
+	<img src="{{ asset('themes/' . $activeTheme . '/assets/not_used/DistrictSeal.png') }}">
 
 	<div class="margin-top-lg">
 		<a href="/social/login" class="btn btn-primary btn-block">{{ trans('kotoba::button.log_in') }}</a>
